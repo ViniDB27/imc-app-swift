7761847a6c4c4bcf25de9cabfc0fdf804ce9cc82
vinicios deivid brejinski <viniciosdb@gmail.com>
2022-05-02 17:32:18 -0300

    app imc swift

--- IMC/ViewController.swift
+++ IMC/ViewController.swift
@@ -15,5 +15,46 @@ class ViewController: UIViewController {
     }
 
 
+
+    @IBOutlet weak var tfWeight: UITextField!
+    @IBOutlet weak var tfHeight: UITextField!
+    @IBOutlet weak var ivResult: UIImageView!
+    @IBOutlet weak var lbResult: UILabel!
+
+    var imc: Double = 0
+    
+    @IBAction func calculate(_ sender: Any) {
+        if let weight =  Double(tfWeight.text!), let height = Double(tfHeight.text!) {
+            imc = weight / (height * height)
+            showResults()
+        }
+    }
+    
+    func showResults() {
+        var result: String = ""
+        var image: String = ""
+        
+        switch imc {
+            case 0..<16:
+                result = "Magreaa"
+                image = "magreza"
+            case 16..<18.5:
+                result = "Abaixo do peso"
+                image = "abaixo"
+            case 18.5..<25:
+                result = "Peso ideal"
+                image = "ideal"
+            case 25..<30:
+                result = "Sobre peso"
+                image = "sobre"
+            default:
+                result = "Obesidade"
+                image = "obesidade"
+        }
+        
+        lbResult.text = result
+        ivResult.image = UIImage(named: image)
+    }
 }
+
 
